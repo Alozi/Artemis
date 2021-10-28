a77f2f8a645f0fe23472de91f1f6480f6f2a5144
Alozi <Alozimozi@gmail.com>
2021-10-28 22:42:50 +0300

    third day of work
    add header and create animations

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Slider */
+
 let width = document.querySelector('video').offsetWidth + 25; // ширина картинки
 console.log(width);
 let count = 3;
@@ -18,3 +20,20 @@ carousel.querySelector('.next').onclick = function() {
   position = Math.max(position, -width * (listElems.length - count));
   list.style.marginLeft = position + 'px';
 };
+
+/* Burger menu */
+document.getElementById('burger-button').onclick = function() {
+  document.getElementById('burger-menu').classList.toggle('navigation__wrapper--open');
+}
+
+document.getElementById('submenu-button').onclick = function() {
+  document.getElementById('submenu').classList.toggle('submenu--open');
+}
+
+document.getElementById('submenu-languages-button').onclick = function() {
+  document.getElementById('submenu-languages').classList.toggle('submenu-languages--open');
+}
+
+
+
+document.querySelectorAll('.button--animation').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
